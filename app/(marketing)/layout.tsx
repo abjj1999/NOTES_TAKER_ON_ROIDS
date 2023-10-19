368d95cfa8bca92daf6684c7dc2364f72a6446a2
Abdullah al hilfi <abdullahalhilfi21@gmail.com>
2023-10-18 22:46:48 -0500

    theme created and dark mode works

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -6,9 +6,9 @@ const MarketingLayout = ({
     children: React.ReactNode;
 }) => {
     return ( 
-        <div className="h-full">
+        <div className="h-full dark:bg-[#131313]">
             <Navbar />
-            <main className="h-full pt-40">
+            <main className="h-full pt-40 ">
                 {children}
             </main>
         </div>
